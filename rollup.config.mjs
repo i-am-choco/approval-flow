d464e7c2974ca515177e52620594031177d46c66
Lusia.L <lusia.l@LusiaL.local>
2024-01-12 13:10:03 +0800

    chore: 整理代码

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -16,6 +16,11 @@ export default [
   input: "./src/index.ts",
     output: [
       {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
         file:  pkg.module,
         format: "esm",
         sourcemap: true,
