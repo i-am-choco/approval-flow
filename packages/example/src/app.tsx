dee34feb21747e8cda1b1fcbb591457f52514e3b
choco <chocoleong@qq.com>
2024-02-21 13:28:35 +0800

    demo: 自定义表单演示项目

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -3,16 +3,18 @@ import "./app.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { Demo } from "./demo/demo";
-import { Flow } from "./demo/flow";
+// import { Demo } from "./demo/demo";
+// import { Flow } from "./demo/flow";
+import { Form } from "./demo/form";
 
 export const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Demo />} />
-          <Route path="/flow/:id" element={<Flow />} />
+          {/* <Route path="/" element={<Demo />} /> */}
+          {/* <Route path="/flow/:id" element={<Flow />} /> */}
+          <Route path="/form" element={<Form />} />
         </Routes>
       </BrowserRouter>
     </div>
